dfecc827639885118d2a087c0912edd34783df92
Umar Khalid <umar.qur105@gmail.com>
2021-04-16 02:41:26 +0500

    toolbar added. Edit and delete icons functionality added.

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -1,48 +1,110 @@
-import React,{Component} from "react";
-import Table from './../common/table';
-import { Link, withRouter } from 'react-router-dom';
-
+import React, { Component } from "react";
+import Table from "./../common/table";
+import { Link, withRouter } from "react-router-dom";
 
 class UsersTable extends Component {
-  columns = [
-    //   {path: '_id', label: 'Id'},
-    {key:'avatar', label: 'avatar',content: user=>(	<span className="icon-img sm-r-5"><img style={{width: "15px",height: "15px"}} src={user.imageSrc} alt="" /></span>) } ,    
-    {label: 'Username',   path: 'username' } ,
-    {label: 'email',   path: 'email' } ,   
-    {label: 'Usergroup',   path: 'profile.profileName' } ,   
-    {label: 'Firstname',   path: 'contactName.first' } ,   
-    {label: 'initials',   path: 'contactName.initials' } ,   	  
-    {label: 'Lastname',   path: 'contactName.last' } ,   	  
-    {label: 'DOB',   path: 'dateBirth' } ,   	  
-    {label: 'Gender',   path: 'gender' } ,   	  
-    {label: 'Address 1',   path: 'address1' } ,   	  	  
-    {label: 'Address 2',   path: 'address2' } ,   	  	  	
-    {label: 'Address 3',   path: 'address3' } ,   	  	  		
-    {label: 'Zip-code',   path: 'zip' } ,   	  	  		
-    {label: 'City',   path: 'city' } ,   	  	  		
-    {label: 'State',   path: 'state' } ,   	  	  		
-    {label: 'Country',   path: 'country' } ,   	  	  
-    {label: 'Mobile',   path: 'mobile' } ,   	  
-    {label: 'Phone',   path: 'phone' } ,   	  
-    {label: 'Skype',   path: 'skype' } , 
-    
-    {key:'edit',label: '',content: user => <Link to={`/clinic/users/${user._id}`}><i className="far fa-lg fa-fw m-r-10 fa-edit"></i></Link>},
-    {key:'delete',label: '',content: user=>(<a onClick={() => this.props.onDelete(user)}><i className="fas fa-lg fa-fw m-r-10 fa-trash"></i></a> )}
-   
-    ];
-   
-    render(){ 
-       //console.log(this.columns) ;
-        const {users,onSort,sortColumn} = this.props;
-        return (
-       <Table columns={this.columns}
-       sortColumn={sortColumn} 
-       onSort={onSort}
-       data={users}
-       />
-        );
-    }
-}
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		values: [],
+	// 	};
+	// }
 
+	columns = [
+		//   {path: '_id', label: 'Id'},
+		{
+			key: "checkbox",
+			label: (
+				<input
+					type="check"
+					style={{
+						width: "15px",
+						height: "15px",
+						marginTop: "0.4rem",
+						borderRadius: 0,
+					}}
+				/>
+			),
+			content: (user) => (
+				<span className="icon-img sm-r-5" style={{ marginTop: "15px" }}>
+					<input
+						type="checkbox"
+						style={{
+							width: "15px",
+							height: "15px",
+							marginTop: "0.4rem",
+							borderRadius: 0,
+						}}
+						onChange={this.props.handleCheckboxChange}
+						value={user._id}
+					/>
+				</span>
+			),
+		},
+		{
+			key: "avatar",
+			label: "avatar",
+			content: (user) => (
+				<span className="icon-img sm-r-5">
+					<img
+						style={{ width: "30px", height: "30px", borderRadius: "50%" }}
+						src={user.imageSrc}
+						alt=""
+					/>
+				</span>
+			),
+		},
+		{ label: "Username", path: "username" },
+		{ label: "email", path: "email" },
+		{ label: "Usergroup", path: "profile.profileName" },
+		{ label: "Firstname", path: "contactName.first" },
+		{ label: "initials", path: "contactName.initials" },
+		{ label: "Lastname", path: "contactName.last" },
+		{ label: "DOB", path: "dateBirth" },
+		{ label: "Gender", path: "gender" },
+		{ label: "Address 1", path: "address1" },
+		{ label: "Address 2", path: "address2" },
+		{ label: "Address 3", path: "address3" },
+		{ label: "Zip-code", path: "zip" },
+		{ label: "City", path: "city" },
+		{ label: "State", path: "state" },
+		{ label: "Country", path: "country" },
+		{ label: "Mobile", path: "mobile" },
+		{ label: "Phone", path: "phone" },
+		{ label: "Skype", path: "skype" },
+
+		{
+			key: "edit",
+			label: "",
+			content: (user) => (
+				<Link to={`/clinic/users/${user._id}`}>
+					<i className="far fa-lg fa-fw m-r-10 fa-edit"></i>
+				</Link>
+			),
+		},
+		{
+			key: "delete",
+			label: "",
+			content: (user) => (
+				<a onClick={() => this.props.onDelete(user)}>
+					<i className="fas fa-lg fa-fw m-r-10 fa-trash"></i>
+				</a>
+			),
+		},
+	];
+
+	render() {
+		//console.log(this.columns) ;
+		const { users, onSort, sortColumn } = this.props;
+		return (
+			<Table
+				columns={this.columns}
+				sortColumn={sortColumn}
+				onSort={onSort}
+				data={users}
+			/>
+		);
+	}
+}
 
 export default UsersTable;
--- src/pages/clinic/users.js
+++ src/pages/clinic/users.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 //import { Link } from 'react-router-dom';
 import { Link, withRouter } from "react-router-dom";
-
 import {
 	Panel,
 	PanelHeader,
@@ -27,6 +26,20 @@ import { apiUrl } from "./../../config/config.json";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import newIcon from "../../assets/Icons/new.svg";
+import editIcon from "../../assets/Icons/edit.svg";
+import deleteIcon from "../../assets/Icons/permanentdelete.svg";
+import csvIcon from "../../assets/Icons/csv.svg";
+import pdfIcon from "../../assets/Icons/pdf.svg";
+import printIcon from "../../assets/Icons/printer-xxl.svg";
+import lockIcon from "../../assets/Icons/lock.svg";
+import unlockIcon from "../../assets/Icons/unlock.svg";
+import banIcon from "../../assets/Icons/ban.svg";
+import emailIcon from "../../assets/Icons/email.svg";
+import messageIcon from "../../assets/Icons/message.svg";
+import xlsIcon from "../../assets/Icons/xls.svg";
+import copy2clipboardIcon from "../../assets/Icons/copy2clipboard.svg";
+
 class UserTableData extends Component {
 	constructor(props) {
 		super(props);
@@ -37,15 +50,37 @@ class UserTableData extends Component {
 			sortColumn: { path: "title", order: "asc" },
 			searchQuery: "",
 			errors: {},
+			checkedUsers: [],
 		};
 
 		this.handleDelete = this.handleDelete.bind(this);
+		this.handleMassDelete = this.handleMassDelete.bind(this);
+		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
 	}
 
 	async componentDidMount() {
 		const { data } = await getUsers();
 		this.setState({ users: data });
 	}
+
+	handleMassDelete = (CheckedUsers) => {
+		const originalUsers = this.state.users;
+		CheckedUsers.map(async (user) => {
+			const users = this.state.users.filter((User) => User._id !== user);
+			// console.log("users: ", users);
+			this.setState({ users });
+			try {
+				await http.delete(apiUrl + "/users/" + user);
+			} catch (ex) {
+				if (ex.response && ex.response === 404) {
+					alert("already deleted");
+				}
+
+				this.setState({ users: originalUsers });
+			}
+			console.log("Users: ", this.state.users);
+		});
+	};
 
 	handleDelete = async (user) => {
 		///delete
@@ -66,6 +101,23 @@ class UserTableData extends Component {
 		}
 		////
 	};
+
+	//check box change
+	handleCheckboxChange = ({ target: { checked, value } }) => {
+		if (checked) {
+			this.setState(({ checkedUsers }) => ({
+				checkedUsers: [...checkedUsers, value],
+			}));
+		} else {
+			this.setState(({ checkedUsers }) => ({
+				checkedUsers: checkedUsers.filter((e) => e !== value),
+			}));
+		}
+		console.log("checked users: ", this.state.checkedUsers);
+	};
+
+	// handle edit
+	handleEdit = (users) => {};
 
 	//sorting columns
 	handleSort = (sortColumn) => {
@@ -132,49 +184,204 @@ class UserTableData extends Component {
 					<React.Fragment>
 						<ToastContainer />
 						{/* {user && ( <button className="btn btn-default active m-r-5 m-b-5" style={{marginBottom:20},{marginLeft:20},{marginTop:20}}>  <Link to="/clinic/user/new">Add User</Link>  </button>)} */}
-						<button
-							className="btn btn-default active m-r-5 m-b-5"
-							title="add user"
-							style={
-								({ marginBottom: 20 },
-								{ marginLeft: 20 },
-								{ marginTop: 20 })
-							}
-						>
-							{" "}
-							<Link to="/clinic/users/new">
-								<i className="fas fa-plus"></i>
-							</Link>{" "}
-						</button>
-						<button
-							className="btn btn-default active m-r-5 m-b-5"
-							title="edit"
-							style={
-								({ marginBottom: 20 },
-								{ marginLeft: 20 },
-								{ marginTop: 20 })
-							}
-						>
-							{" "}
-							<Link to="/clinic/users/edit">
-								<i className="far fa-edit"></i>
-							</Link>{" "}
-						</button>
-						<button
-							className="btn btn-default active m-r-5 m-b-5"
-							title="delete"
-							style={
-								({ marginBottom: 20 },
-								{ marginLeft: 20 },
-								{ marginTop: 20 })
-							}
-						>
-							{" "}
-							<Link to="/clinic/users/del">
-								<i className="far fa-trash-alt"></i>
-							</Link>{" "}
-						</button>
-						<button
+						<div className="toolbar" style={toolbarStyles}>
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="add user"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/new">
+									<img style={iconStyles} src={newIcon} />
+								</Link>{" "}
+							</button>
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="edit"
+								style={btnStyles}
+								// onClick={}
+							>
+								{" "}
+								<Link
+									to={
+										this.state.checkedUsers
+											? `/clinic/users/${this.state.checkedUsers[0]}`
+											: "/clinic/users/"
+									}
+								>
+									<img style={iconStyles} src={editIcon} />
+								</Link>{" "}
+							</button>
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="delete"
+								style={btnStyles}
+								onClick={() =>
+									this.handleMassDelete(this.state.checkedUsers)
+								}
+							>
+								{" "}
+								{/* <Link to="/clinic/users/del"> */}
+								<img
+									style={{ width: "25px", height: "25px" }}
+									src={deleteIcon}
+								/>
+								{/* </Link>{" "} */}
+							</button>
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="csv"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={csvIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="pdf"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={pdfIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="excel"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={xlsIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="print"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={printIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="copy to clipboard"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={copy2clipboardIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="print"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={printIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="print"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={printIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="print"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={printIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="print"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={printIcon} />
+								</Link>{" "}
+							</button>
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="lock"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={lockIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="unlock"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={unlockIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="ban"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={banIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="email"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={emailIcon} />
+								</Link>{" "}
+							</button>
+
+							<button
+								className="btn btn-default active m-r-5 m-b-5"
+								title="message"
+								style={btnStyles}
+							>
+								{" "}
+								<Link to="/clinic/users/">
+									<img style={iconStyles} src={messageIcon} />
+								</Link>{" "}
+							</button>
+						</div>
+						{/* <button
 							className="btn btn-default active m-r-5 m-b-5"
 							title="download"
 							style={
@@ -187,7 +394,8 @@ class UserTableData extends Component {
 							<Link to="/clinic/users/download">
 								<i className="ion-md-download"></i>
 							</Link>{" "}
-						</button>
+						</button> */}
+
 						<div className="table-responsive">
 							<SearchBox
 								value={searchQuery}
@@ -209,6 +417,7 @@ class UserTableData extends Component {
 								onDelete={this.handleDelete}
 								onSort={this.handleSort}
 								sortColumn={sortColumn}
+								handleCheckboxChange={this.handleCheckboxChange}
 							/>
 						</div>
 					</React.Fragment>
@@ -229,5 +438,18 @@ class UserTableData extends Component {
 		);
 	}
 }
+
+const toolbarStyles = {
+	background: "#c8e9f3",
+	padding: "10px",
+};
+
+const btnStyles = { background: "#348fe2", margin: "0rem" };
+
+const iconStyles = {
+	width: "25px",
+	height: "25px",
+	marginRight: "0rem",
+};
 
 export default UserTableData;
