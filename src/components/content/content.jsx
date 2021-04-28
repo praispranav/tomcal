20edfcc9d273a9c35aca9007cc9a770f3a604260
wowtqt <17221995+wowtqt@users.noreply.github.com>
2021-04-28 23:50:58 +0200

    Add files via upload

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -82,25 +82,6 @@ import LoginV2 from '../../pages/user/login.js';
 import RegisterV3 from './../../pages/register.js';
 import User from './../../pages/clinic/user.js';
 import Ticket from "./../../pages/clinic/ticket.js";
-<<<<<<< HEAD
-import reqForAppointment from "./../../pages/clinic/reqforappointment";
-
-
-import ClinicSolo from "./../../pages/clinic/clinicsolo.js";
-import ClinicSoloTableData from "./../../pages/clinic/clinicsolos.js";
-//import Appointment from './../../pages/clinic/appointment.js';
-import AppointmentTableData from "./../../pages/clinic/appointments.js";
-
-//import Patient from './../../pages/clinic/patient.js';
-import PatientTableData from "./../../pages/clinic/patients.js";
-import Patient from "./../../pages/clinic/patient.js";
-import UserTableData from "./../../pages/clinic/users.js";
-import TicketsTableData from "./../../pages/clinic/tickets.js";
-import reqforappointmentTable from "./../../pages/clinic/reqforappointments.js";
-
-import Doctor from "./../../pages/clinic/doctor.js";
-import DoctorTableData from "./../../pages/clinic/doctors.js";
-=======
 import TicketsTable from "./../../pages/clinic/tickets.js";
 import ClinicSolo from './../../pages/clinic/clinicsolo.js';
 import ClinicSoloTable from './../../pages/clinic/clinicsolos.js';
@@ -108,50 +89,35 @@ import ClinicSoloTable from './../../pages/clinic/clinicsolos.js';
 import AppointmentTable from './../../pages/clinic/appointments.js';
 import reqForAppointment from './../../pages/clinic/reqforappointment.js';
 import reqforappointmentsTable from './../../pages/clinic/reqforappointments.js';
-//import Patient from './../../pages/clinic/patient.js';
 import Patient from './../../pages/clinic/patient.js';
 import PatientTable from './../../pages/clinic/patients.js';
 import UserTable from './../../pages/clinic/users.js';
 import Doctor from './../../pages/clinic/doctor.js';
 import DoctorTable from './../../pages/clinic/doctors.js';
->>>>>>> 771abe70dbd53f62a3ef625dc22c6a937923ef45
 //import NewKanBanBoard from './../../pages/clinic/grid-tickets.js';
 //import Label from './../../pages/label/label.js';
 //import LabelTable from './../../pages/label/labels.js';
-//import accountant from './../../pages/clinic/accountant.js';
-//import accountantsTable from './../../pages/clinic/accountants.js';
-// import reception from './../../pages/clinic/reception.js';
-// import receptionTable from './../../pages/clinic/receptions.js';
+import Accountant from './../../pages/clinic/accountant.js';
+import AccountantTable from './../../pages/clinic/accountants.js';
+import Reception from './../../pages/clinic/reception.js';
+import ReceptionTable from './../../pages/clinic/receptions.js';
 // import Invoice from './../../pages/clinic/Invoice.js';
 // import InvoiceTable from './../../pages/clinic/invoices.js';
 // import TCMTreatment from './../../pages/clinic/tcmtreatment.js';
 // import TCMTreatmentTable from './../../pages/clinic/tcmtreatments.js';
 // import MedicalFile from './../../pages/clinic/medicalfile.js';
 // import MedicalFilesTable from './../../pages/clinic/medicalfiles.js';
-<<<<<<< HEAD
-=======
-// import AddTCMSession from './../../pages/clinic/tcmsession.js';
->>>>>>> 771abe70dbd53f62a3ef625dc22c6a937923ef45
-// import AcupunctureTable from './../../pages/clinic/acupuncture.js';
+// import Acupuncture from './../../pages/clinic/acupuncture.js';
 // import AcupunctureTable from './../../pages/clinic/acupunctures.js';
-// import FormulaTable from './../../pages/clinic/formula.js';
+// import Formula from './../../pages/clinic/formula.js';
 // import FormulaTable from './../../pages/clinic/formulas.js';
-// import MateriaMedicaTable from './../../pages/clinic/materiamedica.js';
+// import MateriaMedica from './../../pages/clinic/materiamedica.js';
 // import MateriaMedicaTable from './../../pages/clinic/materiamedicas.js';
 // import MeridianTable from './../../pages/clinic/meridians.js';
 // import Salon from './../../pages/clinic/salon.js';
 // import SalonTable from './../../pages/clinic/salons.js';
 // import BTreatment from './../../pages/clinic/btreatment.js';
 // import BTreatmentTable from './../../pages/clinic/btreatments.js';
-<<<<<<< HEAD
-
-
-// import Card from './../../pages/kanban/card.js';
-// import Listkanban from './../../pages/kanban/listkanban.js';
-// import Kanban from './../../pages/kanban/kanban.js';
-// import Scrumboard from './../../pages/kanban/scrumboard.js';
-=======
->>>>>>> 771abe70dbd53f62a3ef625dc22c6a937923ef45
 
 
 // import Card from './../../pages/kanban/card.js';
@@ -281,159 +247,6 @@ class Content extends React.Component {
               <ProtectedRoute path= '/clinic/users' title="Users" component={UserTable} />
             	<ProtectedRoute path="/clinic/tickets" title="Tickets" component={TicketsTable} />
 
-<<<<<<< HEAD
-								<ProtectedRoute path="/clinic/users" title="Users" component={UserTableData} />
-								<ProtectedRoute path="/clinic/tickets" title="Tickets" component={TicketsTableData} />
-								<ProtectedRoute
-									path="/clinic/reqforappointments"
-									title="ReqForAppointments"
-									component={reqforappointmentTable}
-								/>
-
-								<ProtectedRoute path="/clinic/clinicsolos/:id" title="ClinicSolo" component={ClinicSolo} />
-								<ProtectedRoute
-									path="/clinic/clinicsolos"
-									title="ClinicSolos"
-									component={ClinicSoloTableData}
-								/>
-								<ProtectedRoute path="/clinic/patients/:id" title="Patient" component={Patient} />
-								<ProtectedRoute path="/clinic/patients" title="Patients" component={PatientTableData} />
-								<ProtectedRoute path="/clinic/doctors/:id" title="Doctor" component={Doctor} />
-								<ProtectedRoute path="/clinic/doctors" title="Doctors" component={DoctorTableData} />
-								<ProtectedRoute path="/clinic/clinicsolos/:id" title="ClinicSolo" component={ClinicSolo} />
-								<ProtectedRoute
-									path="/clinic/clinicsolos"
-									title="ClinicSolos"
-									component={ClinicSoloTableData}
-								/>
-								<ProtectedRoute path="/clinic/patients/:id" title="Patient" component={Patient} />
-								<ProtectedRoute path="/clinic/patients" title="Patients" component={PatientTableData} />
-								<ProtectedRoute path="/clinic/doctors/:id" title="Doctor" component={Doctor} />
-								<ProtectedRoute path="/clinic/doctors" title="Doctors" component={DoctorTableData} />
-								<Route path="/clinic/yourdrive/" title="Your Drive" component={YourDrive} />
-								<Route path="/calendar" title="Calendar" component={Calendar} />
-								<Route path="/scheduler" title="Scheduler" component={SchedulerCal} />
-								<Route path="/scheduler2" title="Scheduler2" component={SchedulerfCal} />
-								<Route path="/kanban/kanban" title="KanBanBoard" component={KanBanBoard} />
-								{/* <Route path="/clinic/tickets" title="NewKanBanBoard" component={TicketsTableData} /> */}
-								<Route path="/clinic/add_tcmsession" title="TCM Session" component={TCMSession} />
-								<Route path="/email/inbox" title="Email Inbox" component={EmailInbox} />
-								<Route path="/email/compose" title="Email Compose" component={EmailCompose} />
-								<Route path="/email/detail" title="Email Detail" component={EmailDetail} />
-								<Route path="/widgets" title="Widgets" component={Widgets} />
-								<Route path="/ui/general" title="UI General" component={UIGeneral} />
-								<Route path="/ui/typography" title="UI Typography" component={UITypography} />
-								<Route path="/ui/tabs-accordion" title="UI Tabs Accordion" component={UITabsAccordion} />
-								<Route
-									path="/ui/modal-notification"
-									title="UI Modal Notification"
-									component={UIModalNotification}
-								/>
-								<Route path="/ui/widget-boxes" title="UI Widget Boxes" component={UIWidgetBoxes} />
-								<Route path="/ui/media-object" title="UI Media Object" component={UIMediaObject} />
-								<Route path="/ui/buttons" title="UI Buttons" component={UIButtons} />
-								<Route path="/ui/icons" title="UIIcons" component={UIIcons} />
-								<Route path="/ui/simple-line-icons" title="UISimpleLineIcons" component={UISimpleLineIcons} />
-								<Route path="/ui/ionicons" title="UIIonicons" component={UIIonicons} />
-								<Route path="/ui/language-bar-icon" title="UILanguageBarIcon" component={UILanguageBarIcon} />
-								<Route path="/ui/social-buttons" title="UISocialButtons" component={UISocialButtons} />
-								<Route path="/bootstrap-4" title="Bootstrap4" component={Bootstrap4} />
-								<Route path="/form/elements" title="FormElements" component={FormElements} />
-								<Route path="/form/wizards" title="FormWizards" component={FormWizards} />
-								<Route path="/form/form" title="FormPlugins" component={FormPlugins} />
-								<Route path="/table/basic" title="TableBasic" component={TableBasic} />
-								<Route path="/table/data" title="TableData" component={TableData} />
-								<Route path="/pos/customer-order" title="PosCustomerOrder" component={PosCustomerOrder} />
-								<Route path="/pos/kitchen-order" title="PosKitchenOrder" component={PosKitchenOrder} />
-								<Route path="/pos/counter-checkout" title="PosCounterCheckout" component={PosCounterCheckout} />
-								<Route path="/pos/table-booking" title="PosTableBooking" component={PosTableBooking} />
-								<Route path="/pos/menu-stock" title="PosMenuStock" component={PosMenuStock} />
-								<Route path="/chart/js" title="ChartJS" component={ChartJS} />
-								<Route path="/chart/d3" title="ChartD3" component={ChartD3} />
-								<Route path="/chart/apex" title="ChartApex" component={ChartApex} />
-								<Route path="/map" title="Map" component={Map} />
-								<Route path="/gallery" title="Gallery" component={Gallery} />
-								<Route path="/page-option/with-footer" title="PageWithFooter" component={PageWithFooter} />
-								<Route
-									path="/page-option/without-sidebar"
-									title="PageWithoutSidebar"
-									component={PageWithoutSidebar}
-								/>
-								<Route
-									path="/page-option/with-right-sidebar"
-									title="PageWithRightSidebar"
-									component={PageWithRightSidebar}
-								/>
-								<Route
-									path="/page-option/with-minified-sidebar"
-									title="PageWithMinifiedSidebar"
-									component={PageWithMinifiedSidebar}
-								/>
-								<Route
-									path="/page-option/with-two-sidebar"
-									title="PageWithTwoSidebar"
-									component={PageWithTwoSidebar}
-								/>
-								<Route path="/page-option/full-height" title="PageFullHeight" component={PageFullHeight} />
-								<Route
-									path="/page-option/with-wide-sidebar"
-									title="PageWithWideSidebar"
-									component={PageWithWideSidebar}
-								/>
-								<Route
-									path="/page-option/with-light-sidebar"
-									title="PageWithLightSidebar"
-									component={PageWithLightSidebar}
-								/>
-								<Route
-									path="/page-option/with-mega-menu"
-									title="PageWithMegaMenu"
-									component={PageWithMegaMenu}
-								/>
-								<Route path="/page-option/with-top-menu" title="PageWithTopMenu" component={PageWithTopMenu} />
-								<Route
-									path="/page-option/with-boxed-layout"
-									title="PageWithBoxedLayout"
-									component={PageWithBoxedLayout}
-								/>
-								<Route
-									path="/page-option/with-mixed-menu"
-									title="PageWithMixedMenu"
-									component={PageWithMixedMenu}
-								/>
-								<Route
-									path="/page-option/boxed-layout-with-mixed-menu"
-									title="PageBoxedLayoutWithMixedMenu"
-									component={PageBoxedLayoutWithMixedMenu}
-								/>
-								<Route
-									path="/page-option/with-transparent-sidebar"
-									title="PageWithTransparentSidebar"
-									component={PageWithTransparentSidebar}
-								/>
-								<Route
-									path="/page-option/with-search-sidebar"
-									title="Page With Search Sidebar"
-									component={PageWithSearchSidebar}
-								/>
-								<Route path="/extra/timeline" title="Extra Timeline" component={ExtraTimeline} />
-								<Route path="/extra/coming-soon" title="Extra Coming Soon" component={ExtraComingSoon} />
-								<Route path="/extra/search" title="Extra Search Results" component={ExtraSearch} />
-								<Route path="/extra/invoice" title="Extra Invoice" component={ExtraInvoice} />
-								
-								<Provider store={store}>
-								<Route path="/user/profile" title="Extra Profile" component={Profile} />
-							    </Provider>  
-								<Route path="/extra/scrum-board" title="Extra Scrum Board" component={ExtraScrumBoard} />
-								<Route
-									path="/extra/cookie-acceptance-banner"
-									title="Extra Cookie Acceptance Banner"
-									component={ExtraCookieAcceptanceBanner}
-								/>
-								<Route title="404" component={ExtraError} />
-							</Switch>
-						</React.Fragment>
-=======
               {/* <Route path= '/clinic/users/:id' title="User" render ={props => {
                 if(!this.state.user) return <Redirect to="/user/login" />;
                 return <User {...props} />;
@@ -523,7 +336,6 @@ class Content extends React.Component {
  						 </Switch>
            
               </React.Fragment>
->>>>>>> 771abe70dbd53f62a3ef625dc22c6a937923ef45
 					</div>
 				)
 			}
