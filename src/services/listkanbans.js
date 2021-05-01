ddf6dec3f221bc6d760e811dfd9bbff03eabdcb2
Umar Khalid <umar.qur105@gmail.com>
2021-05-02 02:33:55 +0500

    Update listkanbans.js

--- src/services/listkanbans.js
+++ src/services/listkanbans.js
@@ -1,36 +1,35 @@
-import http from './httpService'; 
-import {apiUrl} from './../config/config.json';
-const apiEndpoint = apiUrl+'/listkanbans';
+import http from "./httpService";
+import { apiUrl } from "./../config/config.json";
+const apiEndpoint = apiUrl + "/listkanbans";
 
+function listkanbanUrl(id) {
+	return `${apiEndpoint}/${id}`;
+}
 
-  function listkanbanUrl(id) {
-    return `${apiEndpoint}/${id}`;
-  }
-  
-  export function getListkanbans() {
-    return http.get(apiEndpoint);
-  }
-  
-  export function getListkanban(Id) {
-    return http.get(listkanbanUrl(Id));
-  }
-  
-  export function saveListkanban(listkanban) {
-    //clone
-    const body = { ...listkanban };
-    console.log(body);
-   //update
-   if (listkanban.id) {
-     //delete _id
-     delete body.id;
-     return http.put(listkanbanUrl(listkanban.id),body);
-   }
- 
-   //add a new listkanban
-   return http.post(apiEndpoint, listkanban);
- }
-  
-  //delete listkanbans
-  export function deleteListkanban(Id) {
-    return http.delete(listkanbanUrl(Id));
-  }  
+export function getListkanbans() {
+	return http.get(apiEndpoint);
+}
+
+export function getListkanban(Id) {
+	return http.get(listkanbanUrl(Id));
+}
+
+export function saveListkanban(listkanban) {
+	//clone
+	const body = { ...listkanban };
+	console.log(body);
+	//update
+	if (listkanban.id) {
+		//delete _id
+		delete body.id;
+		return http.put(listkanbanUrl(listkanban.id), body);
+	}
+
+	//add a new listkanban
+	return http.post(apiEndpoint, listkanban);
+}
+
+//delete listkanbans
+export function deleteListkanban(Id) {
+	return http.delete(listkanbanUrl(Id));
+}
