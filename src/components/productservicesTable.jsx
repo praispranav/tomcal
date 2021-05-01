0cf5333147a50f86151b8b325a394ceed244ecc3
wowtqt <17221995+wowtqt@users.noreply.github.com>
2021-05-01 18:11:20 +0200

    Add files via upload

--- src/components/productservicesTable.jsx
+++ src/components/productservicesTable.jsx
@@ -2,42 +2,39 @@ import React, { Component } from "react";
 import Table from "./../common/table";
 import { Link, withRouter } from "react-router-dom";
 
-class KanbansTable extends Component {
+class ProductservicesTable extends Component {
 	columns = [
 		{
 			key: "checkbox",
 			label: <input type="check" style={checkboxStyles} />,
-			content: (listKanban) => (
+			content: (listProductservice) => (
 				<span className="icon-img sm-r-5" style={{ marginTop: "15px" }}>
 					<input
 						type="checkbox"
 						style={checkboxStyles}
 						onChange={this.props.handleCheckboxChange}
-						value={listKanban._id}
+						value={listProductservice._id}
 					/>
 				</span>
 			),
 		},
-		{label: "KanbanNo", path: "kanbanNo" },
+		{label: "ProductserviceNo", path: "productserviceNo" },
 		{label: "Name", path: "name" },
-		{label: "Owner", path: "username" },
-		{label: "Participants", path: "participants" },
-		{label: "Business", path: "businessName" },		
-		{label: 'Department',   path: "department" } ,   	  
-		{label: 'subDepartment',   path: "subDepartment" } ,   	  		
-		{label: 'Field',   path: "field" } ,   	  
-		{label: 'Tags',   path: "tags" } ,   	  
+		{label: "Code", path: "code" },
+		{label: 'Price',   path: "price" } ,   	  
+		{label: 'Valid Till',   path: "validTill" } ,   	  
 		{label: 'Description',   path: "description" } ,   	  
 		{label: 'Note',   path: "note" } ,   	  	  		
+		{label: "ClinicNo", path: "clinicNo" },		
 		{label: "CreatedOn", path: "createdOn" },
 		{label: "Status", path: "status" },		
 	];
 
 	render() {
 		//console.log(this.columns) ;
-		const { kanbans, onSort, sortColumn } = this.props;
+		const { productservices, onSort, sortColumn } = this.props;
 		return (
-			<Table columns={this.columns} sortColumn={sortColumn} onSort={onSort} data={kanbans} />
+			<Table columns={this.columns} sortColumn={sortColumn} onSort={onSort} data={productservices} />
 		);
 	}
 }
@@ -49,4 +46,4 @@ const checkboxStyles = {
 	borderRadius: 0,
 };
 
-export default KanbansTable;
+export default ProductservicesTable;
